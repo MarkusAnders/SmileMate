3c2aae918eb833b1506e9c12555956c4f6dcc923
MarkusAnders <ser.play02@mail.ru>
2024-04-30 15:25:32 +0300

    Add project

---
+++ src/main/java/ru/dental/smilemate/SmileMateApplication.java
@@ -0,0 +1,13 @@
+package ru.dental.smilemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileMateApplication.class, args);
+	}
+
+}
